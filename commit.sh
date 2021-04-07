f56ece3c7d126aa3a7f23bb3e5d384fbcfe76340
hamuritala <hamuritala@cps.edu>
2021-04-07 19:51:45 +0000

    commit test

---
+++ commit.sh
@@ -0,0 +1,7 @@
+git init
+git add --all
+git config --global user.name "hamuritala"
+git config --global user.email "hamuritala@cps.edu"
+git commit -m "commit test"
+git remote add origin https://github.com/hamuritala/hamuritala.github.io.git
+git push -u origin master
